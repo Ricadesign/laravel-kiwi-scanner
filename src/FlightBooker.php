042bbd1486da4e4c6e1177a15430a432761fc0b1
jperez8 <jperez@rica.design>
2023-03-05 20:12:01 +0100

    feat: return document_need boolean flightcheck

--- src/FlightBooker.php
+++ src/FlightBooker.php
@@ -34,7 +34,7 @@ class FlightBooker
     $checkResult->session_id = $response['session_id'];
     $checkResult->booking_token = $response['booking_token'];
     $checkResult->baggage = $response['baggage'];
-    $checkResult->document_need = $response['document_options']['document_need'];
+    $checkResult->document_need = $response['document_options']['document_need'] != 0;
     return $checkResult;
   }
 
